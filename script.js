471aa93226a6668d134dd0f3e916278097b34a8b
Uzair Javed <uzairjaved75@gmail.com>
2019-06-24 03:22:01 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 var appleQuantity = 0;
 var total = 0;
 var mangoQuantity = 0;
@@ -7,25 +5,38 @@ var bananaQuantity = 0;
 
 function addButtonApple() {
 
-    let accessInput = document.querySelector('#txtNumApple');
+    let wallet = Number(document.querySelector('.wallet').value);
+    let totalBill = Number(document.querySelector('.total-price').innerHTML);
+    if (wallet <= totalBill) {
+        alert('Sorry You have No more Money');
+    }
 
-    appleQuantity++;
+    else {
+        let accessInput = document.querySelector('#txtNumApple');
 
-    accessInput.innerHTML = accessInput.value;
-    accessInput.value = appleQuantity;
-    total = appleQuantity * 10 + mangoQuantity * 15 + bananaQuantity * 7;
-    document.querySelector('.total-price').innerHTML = total;
+        appleQuantity++;
+        total = appleQuantity * 10 + mangoQuantity * 15 + bananaQuantity * 7;
+
+        if (wallet <= totalBill) {
+            alert('Sorry You have No more Money');
+            appleQuantity--;
+        }
+        else {
+            accessInput.innerHTML = accessInput.value;
+            accessInput.value = appleQuantity;
+            document.querySelector('.total-price').innerHTML = total;
+        }
+    }
 }
-
 function substractButtonApple() {
 
     let accessInput = document.querySelector('#txtNumApple');
 
     appleQuantity--;
+    total = appleQuantity * 10 + mangoQuantity * 15 + bananaQuantity * 7;
 
     accessInput.innerHTML = accessInput.value;
     accessInput.value = appleQuantity;
-    total = appleQuantity * 10 + mangoQuantity * 15 + bananaQuantity * 7;
     document.querySelector('.total-price').innerHTML = total;
 }
 
@@ -33,26 +44,41 @@ function substractButtonApple() {
 
 function addButtonMango() {
 
-    let accessInput = document.querySelector('#txtNumMango');
 
-    mangoQuantity++;
+    let wallet = Number(document.querySelector('.wallet').value);
+    let totalBill = Number(document.querySelector('.total-price').innerHTML);
+    if (wallet <= totalBill) {
+        alert('Sorry You have No more Money');
+    }
 
-    accessInput.innerHTML = accessInput.value;
-    accessInput.value = mangoQuantity;
-    total = appleQuantity * 10 + mangoQuantity * 15 + bananaQuantity * 7;
-    document.querySelector('.total-price').innerHTML = total;
+    else {
+
+        let accessInput = document.querySelector('#txtNumMango');
+
+        mangoQuantity++;
+        total = appleQuantity * 10 + mangoQuantity * 15 + bananaQuantity * 7;
+        if (wallet <= total) {
+            alert('Sorry You have No more Money 2');
+            mangoQuantity--;
+        }
+        else {
+            accessInput.innerHTML = accessInput.value;
+            accessInput.value = mangoQuantity;
+            document.querySelector('.total-price').innerHTML = total;
+        }
+
+    }
 }
-
 
 function substractButtonMango() {
 
     let accessInput = document.querySelector('#txtNumMango');
 
     mangoQuantity--;
+    total = appleQuantity * 10 + mangoQuantity * 15 + bananaQuantity * 7;
 
     accessInput.innerHTML = accessInput.value;
     accessInput.value = bananaQuantity;
-    total = appleQuantity * 10 + mangoQuantity * 15 + bananaQuantity * 7;
     document.querySelector('.total-price').innerHTML = total;
 }
 
@@ -60,25 +86,40 @@ function substractButtonMango() {
 
 function addButtonBanana() {
 
-    let accessInput = document.querySelector('#txtNumBanana');
 
-    bananaQuantity++;
+    let wallet = Number(document.querySelector('.wallet').value);
+    let totalBill = Number(document.querySelector('.total-price').innerHTML);
+    if (wallet <= totalBill) {
+        alert('Sorry You have No more Money');
+    }
 
-    accessInput.innerHTML = accessInput.value;
-    accessInput.value = bananaQuantity;
-    total = appleQuantity * 10 + mangoQuantity * 15 + bananaQuantity * 7;
-    document.querySelector('.total-price').innerHTML = total;
+    else {
+        let accessInput = document.querySelector('#txtNumBanana');
+
+        bananaQuantity++;
+        total = appleQuantity * 10 + mangoQuantity * 15 + bananaQuantity * 7;
+
+        if (wallet <= totalBill) {
+            alert('Sorry You have No more Money');
+            bananaQuantity--;
+        }
+
+        else {
+            accessInput.innerHTML = accessInput.value;
+            accessInput.value = bananaQuantity;
+            document.querySelector('.total-price').innerHTML = total;
+        }
+    }
 }
-
 function substractButtonBanana() {
 
     let accessInput = document.querySelector('#txtNumBanana');
 
     bananaQuantity--;
+    total = appleQuantity * 10 + mangoQuantity * 15 + bananaQuantity * 7;
 
     accessInput.innerHTML = accessInput.value;
     accessInput.value = bananaQuantity;
-    total = appleQuantity * 10 + mangoQuantity * 15 + bananaQuantity * 7;
     document.querySelector('.total-price').innerHTML = total;
 }
 
